8402fccd4c3c6bb2c9cca92c8285971635dbde14
Daniel Kostoski <116521337+Dkostoski30@users.noreply.github.com>
2024-11-03 19:53:58 +0100

    Add files via upload

---
+++ Domasna_1/Web Scrapper demo/filter_one.py
@@ -0,0 +1,68 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+import psycopg2
+def has_num(shifra):
+    return any(char.isdigit() for char in shifra)
+
+def fetch_tikeri_bs():
+    url = 'https://www.mse.mk/mk/stats/symbolhistory/kmb'
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Failed to retrieve the page")
+        return []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    dropdown = soup.find('select', {'id': 'Code'})
+    options = dropdown.find_all('option') if dropdown else []
+
+    tikeri = [option['value'] for option in options if not has_num(option['value'])]
+
+    return tikeri
+def insert_into_db(shifri_list):
+    try:
+        # TODO:
+        #Da se smenet argumentive so env promenlivi
+        conn = psycopg2.connect(
+            dbname='postgres',
+            user='postgres',
+            password='1234',
+            host='localhost',
+            port='5432'
+        )
+
+        #TODO:
+        #Da se dodajt uslov da se kreirat tabela Stocks(stock_id, tiker) ako vekje ne postojt
+
+        # TODO:
+        #posle testiranjeto ovie 3 da se trgnet
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Stocks;")
+        conn.commit()
+
+        for shifra in shifri_list:
+            try:
+                cursor.execute("INSERT INTO Stocks (stock_name) VALUES (%s) ON CONFLICT (stock_name) DO NOTHING;", (shifra,))
+            except Exception as e:
+                print(f"Error inserting {shifra}: {e}")
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    except Exception as e:
+        print(f"Database connection error: {e}")
+
+if __name__ == '__main__':
+    tikeri = fetch_tikeri_bs()
+    insert_into_db(tikeri)
---
+++ Domasna_1/Web Scrapper demo/filter_two.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+import psycopg2
+from datetime import datetime, timedelta
+
+NUM_OF_YEARS = 10
+
+def fetch_historic_data_bs4(ticker):
+    base_url = f"https://www.mse.mk/mk/stats/symbolhistory/{ticker}"
+    historic_data = []
+    date_to = datetime.now()
+
+    for _ in range(NUM_OF_YEARS):
+
+        if _ == 0:
+            date_from = date_to - timedelta(days=364)
+        else:
+            date_from = date_to - timedelta(days=365)
+
+        #Dodaj leap year edge cases i nes slicno na to
+
+        params = {
+            "FromDate": date_from.strftime("%d.%m.%Y"),
+            "ToDate": date_to.strftime("%d.%m.%Y"),
+        }
+
+
+        response = requests.get(base_url, params=params)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        table = soup.select_one('#resultsTable > tbody')
+        if table:
+            rows = table.find_all('tr')
+            for row in rows:
+                data_row = [cell.text for cell in row.find_all('td')]
+                historic_data.append(data_row)
+
+
+        date_to = date_from
+
+    return historic_data
+
+
+#TODO: Da se implementirat funkcija za zapisvenje v baza
+
+
+#Optional: thredoj
+
+if __name__ == "__main__":
+    start_time = time.time()
+    df = pd.DataFrame(fetch_historic_data_bs4("adin"),
+                      columns=["Datum", "Posl. trans", "max", "min", "avg", "%", "kol.", "vol", "total vol"])
+    end_time = time.time()
+    print(end_time - start_time)
+    print(df.head())
