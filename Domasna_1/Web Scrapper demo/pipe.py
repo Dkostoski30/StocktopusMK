5a1735f632de6575c9bd371f5f7582903caf92e0
Daniel <kostoskidaniel13@gmail.com>
2024-11-05 20:12:09 +0100

    added checks for datatables in pipe.py

--- Domasna_1/Web Scrapper demo/pipe.py
+++ Domasna_1/Web Scrapper demo/pipe.py
@@ -4,6 +4,23 @@ import psycopg2
 
 import filter_one
 import filter_two
+import filter_two_v2
+
+
+def check_table(table_name, conn):
+
+    table_name = "stocks"
+    exists_query = f"""
+        SELECT EXISTS (
+            SELECT 1 
+            FROM pg_catalog.pg_tables 
+            WHERE schemaname = 'public' 
+            AND tablename = '{table_name}'
+        );
+    """
+    cur = conn.cursor()
+    cur.execute(exists_query)
+    return cur.fetchone()[0]
 
 if __name__ == '__main__':
     conn = psycopg2.connect(
@@ -14,7 +31,11 @@ if __name__ == '__main__':
         port='5432'
     )
     start_time = time.time()
-    tickers = filter_one.init()
-    filter_two.init(tickers, conn)
+    tickers = []
+    if not check_table('stocks', conn):
+        tickers = filter_one.init()
+    if not check_table('stockdetails', conn):
+        filter_two.init(tickers, conn)
     end_time = time.time()
-    print(end_time - start_time)
+    print(f'Time taken from start to finish: {end_time - start_time}')
+    print(f'Script executed..')
