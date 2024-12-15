8c390361b1733987dd2e31eb3dc9ee81a58418a3
Dragana Usovic <draganausovic2@gmail.com>
2024-12-15 15:53:07 +0100

    Add new column date_deleted in stocks

--- Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/repository/StockDetailsRepository.java
+++ Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/repository/StockDetailsRepository.java
@@ -4,7 +4,14 @@ import mk.finki.ukim.mk.stocktopusbackend.model.StockDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockDetailsRepository extends JpaRepository<StockDetails,Long> {
+    @Query("""
+            select sd from StockDetails sd
+            join Stock s on sd.stockId = s.stockId
+            where s.dateDeleted is null
+            ORDER BY sd.date DESC
+            """)
     Page<StockDetails> findAll(Pageable pageable);
 }
--- Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/repository/StockRepository.java
+++ Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/repository/StockRepository.java
@@ -4,6 +4,7 @@ import mk.finki.ukim.mk.stocktopusbackend.model.Stock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,9 @@ import java.util.Optional;
 @Repository
 public interface StockRepository extends JpaRepository<Stock,Long> {
     Optional<Stock> findByStockName(String stockName);
+    @Query("""
+            select s from Stock s
+            where s.dateDeleted is null
+            """)
     Page<Stock> findAll(Pageable pageable);
 }
