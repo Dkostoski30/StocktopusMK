27941027b5a700aa73dff0093337b83c9c220e6f
Dragana Usovic <draganausovic2@gmail.com>
2024-12-16 21:53:03 +0100

    Add API edit stock details  and edit stock details modal on f/e

--- Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/service/StockDetailsService.java
+++ Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/service/StockDetailsService.java
@@ -1,6 +1,8 @@
 package mk.finki.ukim.mk.stocktopusbackend.service;
 
 import mk.finki.ukim.mk.stocktopusbackend.model.StockDetails;
+import mk.finki.ukim.mk.stocktopusbackend.model.dto.StockDetailsDTO;
+import mk.finki.ukim.mk.stocktopusbackend.model.dto.StockDetailsEditDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,4 +10,6 @@ public interface StockDetailsService {
     Page<StockDetails> findAll(Pageable pageable);
 
     void deleteById(Long id);
+
+    StockDetails editStockDetails(Long id, StockDetailsEditDTO stockDetailsDTO);
 }
--- Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/service/impl/StockDetailsServiceImpl.java
+++ Domasna_2/prototip/stocktopus-backend/src/main/java/mk/finki/ukim/mk/stocktopusbackend/service/impl/StockDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package mk.finki.ukim.mk.stocktopusbackend.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import mk.finki.ukim.mk.stocktopusbackend.model.StockDetails;
+import mk.finki.ukim.mk.stocktopusbackend.model.dto.StockDetailsEditDTO;
 import mk.finki.ukim.mk.stocktopusbackend.repository.StockDetailsRepository;
 import mk.finki.ukim.mk.stocktopusbackend.service.StockDetailsService;
 import org.springframework.data.domain.Page;
@@ -24,5 +25,19 @@ public class StockDetailsServiceImpl implements StockDetailsService {
     @Override
     public void deleteById(Long id) {
         stockDetailsRepository.deleteById(id);
+    }
+
+    @Override
+    public StockDetails editStockDetails(Long id, StockDetailsEditDTO stockDetailsDTO) {
+        StockDetails stockDetails = stockDetailsRepository.findById(id).orElseThrow(RuntimeException::new); // TODO: add exception handling
+        stockDetails.setLastTransactionPrice(stockDetailsDTO.lastTransactionPrice());
+        stockDetails.setMaxPrice(stockDetailsDTO.maxPrice());
+        stockDetails.setMinPrice(stockDetailsDTO.minPrice());
+        stockDetails.setAveragePrice(stockDetailsDTO.averagePrice());
+        stockDetails.setPercentageChange(stockDetailsDTO.percentageChange());
+        stockDetails.setQuantity(stockDetailsDTO.quantity());
+        stockDetails.setTradeVolume(stockDetailsDTO.tradeVolume());
+        stockDetails.setTotalVolume(stockDetailsDTO.totalVolume());
+        return stockDetailsRepository.save(stockDetails);
     }
 }
