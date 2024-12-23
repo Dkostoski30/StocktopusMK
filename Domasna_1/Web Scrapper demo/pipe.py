7fbd3cc6a19536204d6d5f45f84c6c549252b5c8
Daniel <kostoskid66@gmail.com>
2024-12-23 15:02:42 +0100

    add scheduler.py

--- Domasna_1/Web Scrapper demo/pipe.py
+++ Domasna_1/Web Scrapper demo/pipe.py
@@ -20,6 +20,73 @@ def check_table(table_name, conn):
 
 global time_taken
 
+def main():
+    time_taken = 0
+    start_time = time.time()
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Stocks (
+            stock_id SERIAL PRIMARY KEY,
+            stock_name VARCHAR(50) UNIQUE
+        );
+    """)
+
+    create_table_sql = """
+        CREATE TABLE IF NOT EXISTS stockdetails (
+                stock_id int NOT NULL,
+                date DATE NOT NULL,
+                last_transaction_price VARCHAR(255),
+                max_price VARCHAR(255),
+                min_price VARCHAR(255),
+                average_price VARCHAR(255),
+                percentage_change VARCHAR(255),
+                quantity VARCHAR(255),
+                trade_volume VARCHAR(255),
+                total_volume VARCHAR(255),
+                PRIMARY KEY (stock_id, date),
+                FOREIGN KEY (stock_id) REFERENCES Stocks(stock_id)
+            );
+    """
+    cursor.execute(create_table_sql)
+    conn.commit()
+    tickers = filter_one.init()
+    conn.close()
+    print('Creating stockdetails table and fetching historic data for each ticker')
+    latest_data = filter_two.init(tickers)
+
+    filter_three.init(latest_data)
+
+    end_time = time.time()
+
+    print(f'Time taken from start to finish: {end_time-start_time:.2f}')
+    print(f'Script executed..')
+
+def scheduled_main():
+    # main() za testiranje
+    while True:
+        now = datetime.datetime.now()
+        target_time = now.replace(hour=14, minute=41, second=0, microsecond=0)
+
+
+        if now > target_time:
+            target_time += datetime.timedelta(days=1)
+
+        time_to_wait = (target_time - now).total_seconds()
+        print(
+            f"Waiting for {time_to_wait // 60:.0f} minutes and {time_to_wait % 60:.0f} seconds to execute the script...")
+
+        time.sleep(time_to_wait)
+
+        main()
+
 if __name__ == '__main__':
     time_taken = 0
     start_time = time.time()
