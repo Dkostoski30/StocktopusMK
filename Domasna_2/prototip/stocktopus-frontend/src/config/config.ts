5e0939e0d4c5586adcaf4c45981de4ab706f5957
Daniel <kostoskidaniel13@gmail.com>
2025-01-17 23:12:15 +0100

    fix config.ts

--- Domasna_2/prototip/stocktopus-frontend/src/config/config.ts
+++ Domasna_2/prototip/stocktopus-frontend/src/config/config.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    API_BASE_URL: `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/api`,
-    PYTHON_BASE_URL: `http://${process.env.PREDICTOR_HOST}:${process.env.PREDICTOR_PORT}`,
+    API_BASE_URL: `http://${import.meta.env.BACKEND_HOST}:${import.meta.env.BACKEND_PORT}/api`,
+    PYTHON_BASE_URL: `http://${import.meta.env.PREDICTOR_HOST}:${import.meta.env.PREDICTOR_PORT}`,
 };
 
 export default config;
