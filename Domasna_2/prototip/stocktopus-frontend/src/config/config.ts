50d5d7a552349ff44a9af7431881326c4689f725
Daniel <kostoskidaniel13@gmail.com>
2025-01-17 22:53:37 +0100

    Update configuration to read from .env file and set actual backend and predictor addresses

--- Domasna_2/prototip/stocktopus-frontend/src/config/config.ts
+++ Domasna_2/prototip/stocktopus-frontend/src/config/config.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
-    API_BASE_URL: "http://localhost:8080/api",
-    PYTHON_BASE_URL: "http://localhost:5000",
+    API_BASE_URL: `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/api`,
+    PYTHON_BASE_URL: `http://${process.env.PREDICTOR_HOST}:${process.env.PREDICTOR_PORT}`,
 };
 
 export default config;
